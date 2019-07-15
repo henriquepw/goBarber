79ddbe6424d2206fab6b2d608d02d774e5996d5d
Henrique Martins <enrque.ns@gmail.com>
2019-07-15 18:35:43 -0300

    redux config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
 import './config/ReactotronConfig';
 
 import Routes from './routes';
 import history from './services/history';
+import store from './store';
 
 import GlobalStyles from './styles/glogal';
 
 function App() {
   return (
-    <Router history={history}>
-      <Routes />
-      <GlobalStyles />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes />
+        <GlobalStyles />
+      </Router>
+    </Provider>
   );
 }
 
