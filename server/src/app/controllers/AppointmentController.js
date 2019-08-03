797fc7bab19d56925c2b08b2fb26108985489870
Henrique Miranda <enrque.ns@gmail.com>
2019-08-03 11:59:30 -0300

    refactoring :hammer:: abstracting controllers

--- server/src/app/controllers/AppointmentController.js
+++ server/src/app/controllers/AppointmentController.js
@@ -1,12 +1,8 @@
-import { isBefore, subHours } from 'date-fns';
-
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
 
-import CancellationMail from '../jobs/CancellationMail';
-import Queue from '../../lib/Queue';
-
+import CancelAppointmentService from '../services/CancelAppointmentService';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
 class AppointmentController {
@@ -42,7 +38,7 @@ class AppointmentController {
     const { provider_id, date } = req.body;
 
     const appointment = await CreateAppointmentService.run({
-      user_id: req.user_id,
+      user_id: req.userId,
       provider_id,
       date,
     });
@@ -51,39 +47,10 @@ class AppointmentController {
   }
 
   async delete(req, res) {
-    const appointment = await Appointment.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          as: 'provider',
-          attributes: ['name', 'email'],
-        },
-        {
-          model: User,
-          as: 'user',
-          attributes: ['name'],
-        },
-      ],
+    const appointment = await CancelAppointmentService.run({
+      provider_id: req.params.id,
+      user_id: req.userId,
     });
-
-    if (appointment.user_id !== req.userId) {
-      return res.status(401).json({
-        error: "You don't have permission to cancel this appointment",
-      });
-    }
-
-    const deadline = subHours(appointment.date, 2);
-    if (isBefore(deadline, new Date())) {
-      return res.status(401).json({
-        error: 'You can only cancel appointments 2 hours is advance',
-      });
-    }
-
-    appointment.canceled_at = new Date();
-
-    await appointment.save();
-
-    await Queue.add(CancellationMail.key, { appointment });
 
     return res.json(appointment);
   }
