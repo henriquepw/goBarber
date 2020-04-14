23704b7f5545f9de0e2b47ba352652f53b27fe35
Henrique Miranda <enrque.ns@gmail.com>
2020-04-14 11:57:46 -0300

    :tada: re-init the project

--- server/src/config/auth.js
+++
@@ -1,4 +0,0 @@
-export default {
-  secret: process.env.APP_SECRETE,
-  expiresIn: '7d',
-};
--- server/src/server.js
+++
@@ -1,4 +0,0 @@
-import 'dotenv/config';
-import app from './app';
-
-app.listen(process.env.SERVER_PORT || 3333);
