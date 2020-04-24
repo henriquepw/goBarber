5fa5f684ce86e1691d30f80758014d5415ba4cbf
Henrique Miranda <enrque.ns@gmail.com>
2020-04-24 15:59:49 -0300

    :sparkles: feat(mobile): create routes

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,10 +1,16 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
-import { View, Text } from 'react-native';
+import { StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from './routes';
 
 const App: React.FC = () => (
-  <View>
-    <Text>Hello</Text>
-  </View>
+  <NavigationContainer>
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+    <Routes />
+  </NavigationContainer>
 );
 
 export default App;
