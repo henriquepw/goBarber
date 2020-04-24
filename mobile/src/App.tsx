125f686d9fdf584480b76fa7ebb350642909e123
Henrique Miranda <enrque.ns@gmail.com>
2020-04-24 12:37:41 -0300

    :tada: re-init mobile

---
+++ mobile/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App: React.FC = () => (
+  <View>
+    <Text>Hello</Text>
+  </View>
+);
+
+export default App;
--- mobile/src/services/api.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'http://192.168.0.198:3333',
-});
