ba72091b97608e5079a96f0337a0de0679a019a2
Henrique Miranda <enrque.ns@gmail.com>
2020-05-04 12:26:45 -0300

    :hammer: refact(server): dependency inversion on user repository

--- server/src/modules/users/services/CreateUserService.ts
+++ server/src/modules/users/services/CreateUserService.ts
@@ -1,23 +1,21 @@
-import { getRepository } from 'typeorm';
 import { hash } from 'bcryptjs';
 
 import AppError from '@shared/errors/AppError';
 
 import User from '../infra/typeorm/entities/User';
+import IUsersRepository from '../repositories/IUsersRepository';
 
-interface Request {
+interface IRequest {
   name: string;
   email: string;
   password: string;
 }
 
 class CreateUserService {
-  public async execute({ name, email, password }: Request): Promise<User> {
-    const usersRepository = getRepository(User);
+  constructor(private usersRepository: IUsersRepository) {}
 
-    const userExists = await usersRepository.findOne({
-      where: { email },
-    });
+  public async execute({ name, email, password }: IRequest): Promise<User> {
+    const userExists = await this.usersRepository.findByEmail(email);
 
     if (userExists) {
       throw new AppError('Email address already used.');
@@ -25,13 +23,11 @@ class CreateUserService {
 
     const passwordHash = await hash(password, 8);
 
-    const user = usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password: passwordHash,
     });
-
-    await usersRepository.save(user);
 
     return user;
   }
--- server/src/modules/users/services/UpdateUserAvatarService.ts
+++ server/src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,24 +1,23 @@
 import path from 'path';
 import fs from 'fs';
 
-import { getRepository } from 'typeorm';
-
 import uploadConfig from '@config/upload';
 
 import AppError from '@shared/errors/AppError';
 
 import User from '../infra/typeorm/entities/User';
+import IUsersRepository from '../repositories/IUsersRepository';
 
-interface Request {
+interface IRequest {
   userId: string;
   avatarFilename: string;
 }
 
 class UpdateUserAvatarService {
-  public async execute({ userId, avatarFilename }: Request): Promise<User> {
-    const usersRepository = getRepository(User);
+  constructor(private usersRepository: IUsersRepository) {}
 
-    const user = await usersRepository.findOne(userId);
+  public async execute({ userId, avatarFilename }: IRequest): Promise<User> {
+    const user = await this.usersRepository.findById(userId);
 
     if (!user) {
       throw new AppError('Only authenticated users can change avatar.', 401);
@@ -36,7 +35,7 @@ class UpdateUserAvatarService {
 
     user.avatar = avatarFilename;
 
-    usersRepository.save(user);
+    this.usersRepository.save(user);
 
     return user;
   }
