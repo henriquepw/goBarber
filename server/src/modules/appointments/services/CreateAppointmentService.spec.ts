a4952362ce8ff4c3ea93405cf69a1c0b48e59fa7
Henrique Miranda <enrque.ns@gmail.com>
2020-05-04 17:45:29 -0300

    :bulb: chore(server): config jest

---
+++ server/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,7 @@
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it.todo('should be able to create a new appointment');
+
+  it.todo('should not be able to create two appointment on the same time');
+});
