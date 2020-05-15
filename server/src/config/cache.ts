af8daa7f2b72b2a3247a8b6ac7bdfecfd9949098
Henrique Miranda <enrque.ns@gmail.com>
2020-05-15 00:27:23 -0300

    :sparkles: feat(server): apply rate limit

--- server/src/config/cache.ts
+++ server/src/config/cache.ts
@@ -11,9 +11,9 @@ export default {
   driver: 'redis',
   config: {
     redis: {
-      port: 6379,
-      host: '127.0.0.1',
-      password: undefined,
+      port: process.env.REDIS_PORT,
+      host: process.env.REDIS_HOST,
+      password: process.env.REDIS_PASS || undefined,
     },
   },
 } as ICacheConfig;
--- server/src/shared/infra/http/server.ts
+++ server/src/shared/infra/http/server.ts
@@ -9,6 +9,8 @@ import { errors } from 'celebrate';
 
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
+import rateLimiter from './middlewares/RateLimiter';
+
 import routes from './routes';
 
 import '@shared/infra/typeorm';
@@ -17,6 +19,7 @@ import '@shared/container';
 const app = express();
 
 app.use(cors());
+app.use(rateLimiter);
 app.use(express.json());
 
 app.use('/files', express.static(uploadConfig.uploadsFolder));
