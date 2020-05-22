54ddc350c81a000f7d0b549bc33525d8d63759f7
Henrique Miranda <enrque.ns@gmail.com>
2020-05-22 13:12:50 -0300

    :bulb: chore(server): cache serialization

--- server/src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ server/src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { classToClass } from 'class-transformer';
 import { container } from 'tsyringe';
 
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
@@ -15,7 +16,7 @@ class AppointmentsController {
       providerId,
     });
 
-    return res.json(appointment);
+    return res.json(classToClass(appointment));
   }
 }
 
--- server/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ server/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,3 +1,4 @@
+import { classToClass } from 'class-transformer';
 import { injectable, inject } from 'tsyringe';
 
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
@@ -44,7 +45,7 @@ class ListProviderAppointmentsService {
         },
       );
 
-      await this.cacheProvider.save(cacheKey, appointments);
+      await this.cacheProvider.save(cacheKey, classToClass(appointments));
     }
 
     return appointments;
