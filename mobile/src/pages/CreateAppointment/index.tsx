5b84a351cdbb0a687de34e1bb69774216d0b7b85
Henrique Miranda <enrque.ns@gmail.com>
2020-06-12 11:35:15 -0300

    :bulb: chore(mobile): toggle provider on create appointment screen

--- mobile/src/pages/CreateAppointment/index.tsx
+++ mobile/src/pages/CreateAppointment/index.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRoute, useNavigation } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Feather';
 
 import { useAuth } from '../../hooks/auth';
+
+import api from '../../services/api';
 
 import {
   Container,
@@ -10,19 +12,46 @@ import {
   BackButton,
   HeaderTitle,
   UserAvatar,
+  ProvidersListContainer,
+  ProvidersList,
+  ProviderContainer,
+  ProviderAvatar,
+  ProviderName,
 } from './styles';
 
 interface RouteParams {
   providerId: string;
 }
 
+export interface Provider {
+  id: string;
+  name: string;
+  avatarUrl: string;
+}
+
 const CreateAppointment: React.FC = () => {
   const { goBack } = useNavigation();
 
   const route = useRoute();
-  const { providerId } = route.params as RouteParams;
+
+  const routeParams = route.params as RouteParams;
+  const [selectedProvider, setSeletectedProvier] = useState(
+    routeParams.providerId,
+  );
 
   const { user } = useAuth();
+
+  const [providers, setProviders] = useState<Provider[]>([]);
+
+  useEffect(() => {
+    async function loadingProviders(): Promise<void> {
+      const response = await api.get('providers');
+
+      setProviders(response.data);
+    }
+
+    loadingProviders();
+  }, []);
 
   return (
     <Container>
@@ -35,6 +64,26 @@ const CreateAppointment: React.FC = () => {
 
         <UserAvatar source={{ uri: user.avatarUrl }} />
       </Header>
+
+      <ProvidersListContainer>
+        <ProvidersList
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          data={providers}
+          keyExtractor={(provider) => provider.id}
+          renderItem={({ item }) => (
+            <ProviderContainer
+              selected={selectedProvider === item.id}
+              onPress={() => setSeletectedProvier(item.id)}
+            >
+              <ProviderAvatar source={{ uri: item.avatarUrl }} />
+              <ProviderName selected={selectedProvider === item.id}>
+                {item.name}
+              </ProviderName>
+            </ProviderContainer>
+          )}
+        />
+      </ProvidersListContainer>
     </Container>
   );
 };
--- mobile/src/pages/CreateAppointment/styles.ts
+++ mobile/src/pages/CreateAppointment/styles.ts
@@ -1,4 +1,12 @@
 import styled from 'styled-components/native';
+import { FlatList } from 'react-native';
+
+import { RectButton } from 'react-native-gesture-handler';
+import { Provider } from './index';
+
+interface ProviderProps {
+  selected?: boolean;
+}
 
 export const Container = styled.View`
   flex: 1;
@@ -29,4 +37,43 @@ export const UserAvatar = styled.Image`
 
   border-radius: 28px;
   margin-left: auto;
+`;
+
+export const ProvidersListContainer = styled.View`
+  height: 112px;
+`;
+
+export const ProvidersList = styled(FlatList as new () => FlatList<Provider>)`
+  padding: 32px 24px;
+`;
+
+export const ProviderContainer = styled(RectButton)<ProviderProps>`
+  flex-direction: row;
+  align-items: center;
+
+  background: ${(props) =>
+    props.selected ? props.theme.colors.active : '#3e3b47'};
+
+  padding: 8px 12px;
+  margin-right: 16px;
+  border-radius: 10px;
+`;
+
+export const ProviderAvatar = styled.Image`
+  width: 32px;
+  height: 32px;
+
+  border-radius: 16px;
+`;
+
+export const ProviderName = styled.Text<ProviderProps>`
+  color: ${(props) =>
+    props.selected
+      ? props.theme.colors.secundaryBackground
+      : props.theme.colors.secundaryText};
+
+  font-family: 'RobotoSlab-Medium';
+  font-size: 16px;
+
+  margin-left: 8px;
 `;
